a8dc01183654f3e11473f2ded149d6326fa3c7bd
SantiagoECCI1 Varela <santiago.varelah@ecci.edu.co>
2024-08-31 00:36:46 +0000

    CREATED 01_app.py

---
+++ Ejemplos/01_app.py
@@ -0,0 +1,16 @@
+import requests 
+import json
+
+url = 'http://localhost:11434/api/generate'
+
+data = {
+    "model": "tinyllama",
+    "prompt": "Why the sky is blue?",
+    "stream": False
+}
+
+response = requests.post(url, json = data)
+
+response = json.loads(response.text)
+
+print(response["response"])
